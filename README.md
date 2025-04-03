f797f64bc85cbdbc3a075feedc088819f056b0c1
Fuma Nama <god63820869@gmail.com>
2025-04-03 16:15:50 +0800

    add details

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Proposal Only
 
 Code is only for reference.
+
+See `app/blog/[slug]/page.tsx` for accessing collections, `content.ts` for declaring collections.
--- content.ts
+++ content.ts
@@ -1,18 +1,24 @@
 import FastGlob from "fast-glob";
 import { defineCollection } from "next/content";
+import fs from "node:fs/promises";
 import path from "node:path";
 
 export const blog = defineCollection({
-    async scan({ hmr, computed }) {
-        const files = await FastGlob('./content/blog/*.mdx')
-        hmr.watchDir('content/blog')
+  async scan({ hmr, computed }) {
+    const files = await FastGlob("./content/blog/*.mdx");
+    hmr.watchDir("content/blog");
 
-        return files.map(file => ({
-            path: file,
-            slug: path.basename(file, path.extname(file)),
-            compile: computed(async () => {
-                return "compiled!"
-            })
-        }))
-    },
-})
+    return files.map((file) => ({
+      path: file,
+      // slug
+      slug: path.basename(file, path.extname(file)),
+      compile: computed(async () => {
+        // this will be re-computed when hmr trigger
+        // e.g. you updated file content
+        const content = (await fs.readFile(file)).toString();
+
+        return `compiled: ${content}`;
+      }),
+    }));
+  },
+});
